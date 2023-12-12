ddb88e07476c2f616b34056a3c40ac442508f4be
hir4eth <asafonov@timetta.ru>
2023-12-13 01:43:22 +0300

    feat: auth improvements

--- Casino.Passport/Program.cs
+++ Casino.Passport/Program.cs
@@ -1,5 +1,17 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+var myAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
 
+
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    })
+    .AddCookie(options => { options.LoginPath = "/login"; })
+    .AddSteam();
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -7,21 +19,38 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddAuthentication().AddSteam();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: myAllowSpecificOrigins,
+        policy =>
+        {
+            policy.WithOrigins("https://steamcommunity.com")
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+});
+
 
 var app = builder.Build();
+
+app.UseCors(myAllowSpecificOrigins);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-	app.UseSwagger();
-	app.UseSwaggerUI();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+//Тестовая хуйня, выводящая в браузере ссылку с SteamId, если ты авторизован
+app.MapGet("/test", (ClaimsPrincipal user) => user.Claims.First().Value)
+    .RequireAuthorization();
 
 app.Run();
