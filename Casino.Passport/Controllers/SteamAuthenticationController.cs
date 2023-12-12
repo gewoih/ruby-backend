ad7f1cf77675afd2010c4a503b3d9ece80903dea
Nikita Ranenko <nranenko@bk.ru>
2023-12-13 02:45:54 +0700

    feat: steam authentication init

---
+++ Casino.Passport/Controllers/SteamAuthenticationController.cs
@@ -0,0 +1,23 @@
+﻿using IdentityServer4;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Casino.Passport.Controllers
+{
+	[Route("login")]
+	public class SteamAuthenticationController : Controller
+	{
+		[HttpGet]
+		public IActionResult Login()
+		{
+			return Challenge("Steam");
+		}
+
+		[HttpGet("callback")]
+		public async Task<IActionResult> LoginCallback()
+		{
+			var result = await HttpContext.AuthenticateAsync(IdentityServerConstants.ExternalCookieAuthenticationScheme);
+			return Ok();
+		}
+	}
+}
