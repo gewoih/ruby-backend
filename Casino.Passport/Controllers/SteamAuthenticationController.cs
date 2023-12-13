671581fabe00aba173ca3ffb5190149b88092102
Nikita Ranenko <nranenko@bk.ru>
2023-12-13 18:23:56 +0700

    feat: removed excess code

--- Casino.Passport/Controllers/SteamAuthenticationController.cs
+++ Casino.Passport/Controllers/SteamAuthenticationController.cs
@@ -1,41 +1,17 @@
-﻿using System.Security.Claims;
-using IdentityServer4;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.BearerToken;
+﻿using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Casino.Passport.Controllers
 {
-    [Route("login")]
-    public class SteamAuthenticationController : Controller
-    {
-        [HttpGet]
-        public IActionResult Login()
-        {
-            return Challenge(new AuthenticationProperties { RedirectUri = "/test" }, "Steam");
-        }
+	[Route("login")]
+	public class SteamAuthenticationController : Controller
+	{
+		[HttpGet]
+		public IActionResult Login()
+		{
+			var result = Challenge(new AuthenticationProperties { RedirectUri = "/test" }, "Steam");
 
-        //TODO: потом удалить)))
-        //Оставил пока что чтоб было, но оно не нужно ни для чего
-        // [HttpGet("callback")]
-        // public async Task<IActionResult> LoginCallback()
-        // {
-        //     var result = await HttpContext.AuthenticateAsync("Steam");
-        //
-        //     if (result.Succeeded != true) return Ok();
-        //
-        //     var claimsPrincipal = new ClaimsPrincipal(
-        //         new ClaimsIdentity(
-        //             new[]
-        //             {
-        //                 new Claim(ClaimTypes.NameIdentifier,
-        //                     result.Principal.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value)
-        //             },
-        //             BearerTokenDefaults.AuthenticationScheme
-        //         )
-        //     );
-        //
-        //     return SignIn(claimsPrincipal);
-        // }
-    }
+			return result;
+		}
+	}
 }
--- Casino.Passport/Program.cs
+++ Casino.Passport/Program.cs
@@ -1,10 +1,7 @@
 using System.Security.Claims;
 using Microsoft.AspNetCore.Authentication.Cookies;
 
-var myAllowSpecificOrigins = "_myAllowSpecificOrigins";
-
 var builder = WebApplication.CreateBuilder(args);
-
 
 builder.Services.AddAuthentication(options =>
     {
@@ -12,30 +9,13 @@ builder.Services.AddAuthentication(options =>
     })
     .AddCookie(options => { options.LoginPath = "/login"; })
     .AddSteam();
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy(name: myAllowSpecificOrigins,
-        policy =>
-        {
-            policy.WithOrigins("https://steamcommunity.com")
-                .AllowAnyHeader()
-                .AllowAnyMethod();
-        });
-});
-
-
 var app = builder.Build();
 
-app.UseCors(myAllowSpecificOrigins);
-
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -49,7 +29,6 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-//Тестовая хуйня, выводящая в браузере ссылку с SteamId, если ты авторизован
 app.MapGet("/test", (ClaimsPrincipal user) => user.Claims.First().Value)
     .RequireAuthorization();
 
