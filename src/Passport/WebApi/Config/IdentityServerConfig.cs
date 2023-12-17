c944e51a97f55dab0f44b616a5fa16bf30ea10bf
hir4eth <asafonov@timetta.ru>
2023-12-17 17:54:24 +0300

    feat: IdentityServerConfig.cs client secrets removed, logout endpoint

--- src/Passport/WebApi/Config/IdentityServerConfig.cs
+++ src/Passport/WebApi/Config/IdentityServerConfig.cs
@@ -3,59 +3,56 @@ using IdentityServer4.Models;
 
 namespace Casino.Passport.Config
 {
-	public class IdentityServerConfig
-	{
-		public static IEnumerable<IdentityResource> GetIdentityResources()
-		{
-			return new List<IdentityResource>
-			{
-				new IdentityResources.OpenId()
-			};
-		}
+    public class IdentityServerConfig
+    {
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId()
+            };
+        }
 
-		public static IEnumerable<ApiScope> GetApiScopes()
-		{
-			return new List<ApiScope>
-			{
-				new("all", "Full Casino API")
-			};
-		}
+        public static IEnumerable<ApiScope> GetApiScopes()
+        {
+            return new List<ApiScope>
+            {
+                new("all", "Full Casino API")
+            };
+        }
 
-		public static IEnumerable<ApiResource> GetApiResources()
-		{
-			return new List<ApiResource>
-			{
-				new("casino-api", "Casino API")
-				{
-					Scopes = { "all" }
-				}
-			};
-		}
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new("casino-api", "Casino API")
+                {
+                    Scopes = { "all" }
+                }
+            };
+        }
 
-		public static IEnumerable<Client> GetClients()
-		{
-			return new List<Client>
-			{
-				new()
-				{
-					ClientId = "web_app",
-					ClientName = "Casino WebApp",
-					AllowedGrantTypes = GrantTypes.Code,
-
-					ClientSecrets =
-					{
-						new Secret("A43DCC44-AC8C-46EE-B312-ECA6F6CBFA52".Sha256())
-					},
-
-					AllowedScopes =
-					{
-						IdentityServerConstants.StandardScopes.OpenId, "all"
-					},
-					RequirePkce = true,
-					RedirectUris = { "https://localhost:7220/login/callback" },
-					AllowOfflineAccess = true,
-				}
-			};
-		}
-	}
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new()
+                {
+                    ClientId = "web_app",
+                    ClientName = "Casino WebApp",
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RequireClientSecret = false,
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId, "all"
+                    },
+                    RequirePkce = true,
+                    RedirectUris = { "https://localhost:7220/login/callback", "http://localhost:4200" },
+                    AllowedCorsOrigins = { "http://localhost:4200" },
+                    PostLogoutRedirectUris = { "http://localhost:4200" },
+                    AllowOfflineAccess = true,
+                }
+            };
+        }
+    }
 }
--- src/Passport/WebApi/Controllers/AuthenticationController.cs
+++ src/Passport/WebApi/Controllers/AuthenticationController.cs
@@ -1,44 +1,49 @@
-﻿using IdentityModel.Client;
-using IdentityServer4.Extensions;
-using Microsoft.AspNetCore.Authentication;
+﻿using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Casino.Passport.Controllers
 {
-	[Route("login")]
-	public class SteamAuthenticationController : Controller
-	{
-		[HttpGet]
-		public IActionResult Login(string returnUrl)
-		{
-			return Challenge(new AuthenticationProperties
-			{
-				RedirectUri = returnUrl
-			}, "Steam");
-		}
+    public class SteamAuthenticationController : Controller
+    {
+        [HttpGet("login")]
+        public IActionResult Login(string returnUrl)
+        {
+            return Challenge(new AuthenticationProperties
+            {
+                RedirectUri = returnUrl
+            }, "Steam");
+        }
 
-		[HttpGet("callback")]
-		public async Task<IActionResult> Callback(string code, string scope, string session_state)
-		{
-			return Ok();
-			//var tokenResponse = await new HttpClient().RequestAuthorizationCodeTokenAsync(new AuthorizationCodeTokenRequest
-			//{
-			//	Address = "https://localhost:7220/connect/token",
-			//	ClientId = "web_app",
-			//	Code = code,
-			//	CodeVerifier = "f822b246de6bd0d4fe2ffa0723da8a8627a3ccb05fb71b6018cee21a",
-			//	GrantType = "authorization_code",
-			//	ClientSecret = "A43DCC44-AC8C-46EE-B312-ECA6F6CBFA52",
-			//	RedirectUri = "https://localhost:7220/login/callback"
-			//});
+        [HttpGet("logout")]
+        // [ValidateAntiForgeryToken]
+        public IActionResult Logout()
+        {
+            return SignOut(new AuthenticationProperties() { RedirectUri = "http://localhost:4200" }, "Cookies", "idsrv",
+                "idsrv.external");
+        }
 
-			//if (tokenResponse.IsError)
-			//	return BadRequest(tokenResponse.Error);
+        [HttpGet("callback")]
+        public async Task<IActionResult> Callback(string code, string scope, string session_state)
+        {
+            return Ok();
+            //var tokenResponse = await new HttpClient().RequestAuthorizationCodeTokenAsync(new AuthorizationCodeTokenRequest
+            //{
+            //	Address = "https://localhost:7220/connect/token",
+            //	ClientId = "web_app",
+            //	Code = code,
+            //	CodeVerifier = "f822b246de6bd0d4fe2ffa0723da8a8627a3ccb05fb71b6018cee21a",
+            //	GrantType = "authorization_code",
+            //	ClientSecret = "A43DCC44-AC8C-46EE-B312-ECA6F6CBFA52",
+            //	RedirectUri = "https://localhost:7220/login/callback"
+            //});
 
-			//if (HttpContext.User.IsAuthenticated())
-			//	return Ok($"Пользователь аутентифицирован: {tokenResponse.AccessToken}");
-			//else
-			//	return NotFound("Пользователь не прошел аутентификацию!");
-		}
-	}
+            //if (tokenResponse.IsError)
+            //	return BadRequest(tokenResponse.Error);
+
+            //if (HttpContext.User.IsAuthenticated())
+            //	return Ok($"Пользователь аутентифицирован: {tokenResponse.AccessToken}");
+            //else
+            //	return NotFound("Пользователь не прошел аутентификацию!");
+        }
+    }
 }
