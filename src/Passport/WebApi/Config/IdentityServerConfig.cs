46098183d4526bf177e2667620f4006854123abb
Nikita Ranenko <nranenko@bk.ru>
2023-12-19 01:54:16 +0700

    refactor: namespaces synced

--- src/Passport/WebApi/Config/IdentityServerConfig.cs
+++ src/Passport/WebApi/Config/IdentityServerConfig.cs
@@ -1,60 +1,60 @@
 ﻿using IdentityServer4;
 using IdentityServer4.Models;
 
-namespace Casino.Passport.Config
+namespace Passport.WebApi.Config
 {
-    public class IdentityServerConfig
-    {
-        public static IEnumerable<IdentityResource> GetIdentityResources()
-        {
-            return new List<IdentityResource>
-            {
-                new IdentityResources.OpenId()
-            };
-        }
+	public class IdentityServerConfig
+	{
+		public static IEnumerable<IdentityResource> GetIdentityResources()
+		{
+			return new List<IdentityResource>
+			{
+				new IdentityResources.OpenId()
+			};
+		}
 
-        public static IEnumerable<ApiScope> GetApiScopes()
-        {
-            return new List<ApiScope>
-            {
-                new("all", "Full Casino API")
-            };
-        }
+		public static IEnumerable<ApiScope> GetApiScopes()
+		{
+			return new List<ApiScope>
+			{
+				new("all", "Full Casino API")
+			};
+		}
 
-        public static IEnumerable<ApiResource> GetApiResources()
-        {
-            return new List<ApiResource>
-            {
-                new("casino-api", "Casino API")
-                {
-                    Scopes = { "all" }
-                }
-            };
-        }
+		public static IEnumerable<ApiResource> GetApiResources()
+		{
+			return new List<ApiResource>
+			{
+				new("casino-api", "Casino API")
+				{
+					Scopes = { "all" }
+				}
+			};
+		}
 
-        public static IEnumerable<Client> GetClients()
-        {
-            return new List<Client>
-            {
-                new()
-                {
-                    ClientId = "web_app",
-                    ClientName = "Casino WebApp",
-                    AllowedGrantTypes = GrantTypes.Code,
-                    RequireClientSecret = false,
-                    AllowedScopes =
-                    {
-                        IdentityServerConstants.StandardScopes.OpenId, "all"
-                    },
-                    RequirePkce = true,
-                    RedirectUris = { "https://localhost:7220/callback", "http://localhost:4200" },
-                    AllowedCorsOrigins = { "http://localhost:4200" },
-                    PostLogoutRedirectUris = { "http://localhost:4200" },
-                    AllowOfflineAccess = true,
-                    AllowAccessTokensViaBrowser = true,
-                    AccessTokenLifetime = 60
-                }
-            };
-        }
-    }
+		public static IEnumerable<Client> GetClients()
+		{
+			return new List<Client>
+			{
+				new()
+				{
+					ClientId = "web_app",
+					ClientName = "Casino WebApp",
+					AllowedGrantTypes = GrantTypes.Code,
+					RequireClientSecret = false,
+					AllowedScopes =
+					{
+						IdentityServerConstants.StandardScopes.OpenId, "all"
+					},
+					RequirePkce = true,
+					RedirectUris = { "https://localhost:7220/callback", "http://localhost:4200" },
+					AllowedCorsOrigins = { "http://localhost:4200" },
+					PostLogoutRedirectUris = { "http://localhost:4200" },
+					AllowOfflineAccess = true,
+					AllowAccessTokensViaBrowser = true,
+					AccessTokenLifetime = 60
+				}
+			};
+		}
+	}
 }
