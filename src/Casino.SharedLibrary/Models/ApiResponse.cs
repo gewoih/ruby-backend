ea9f9627beff7d9c68f261580520feb8599c3e93
Nikita Ranenko <nranenko@bk.ru>
2023-12-20 21:32:33 +0700

    feat: consistent ApiResponse<T> added

---
+++ src/Casino.SharedLibrary/Models/ApiResponse.cs
@@ -0,0 +1,25 @@
+﻿namespace Casino.SharedLibrary.Models
+{
+	public sealed class ApiResponse<T> where T : class
+	{
+		public bool Succeeded { get; set; }
+		public string Message { get; set; }
+		public T Data { get; set; }
+
+		public ApiResponse<T> Success(T data)
+		{
+			Succeeded = true;
+			Data = data;
+
+			return this;
+		}
+
+		public ApiResponse<T> Error(string message)
+		{
+			Succeeded = false;
+			Message = message;
+
+			return this;
+		}
+	}
+}
--- src/SteamInventory/WebApi/Controllers/InventoryController.cs
+++ src/SteamInventory/WebApi/Controllers/InventoryController.cs
@@ -1,25 +1,31 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using Casino.SharedLibrary.Models;
 using Microsoft.AspNetCore.Mvc;
 using SteamInventory.Application.Models;
-using SteamInventory.Application.Services;
+using SteamInventory.Application.Models.Inventory;
+using SteamInventory.Application.Services.Steam;
+using SteamInventory.Application.Services.Waxpeer;
 
 namespace SteamInventory.WebApi.Controllers
 {
-	[Route("api")]
+    [Route("api")]
 	public class InventoryController : Controller
 	{
 		private readonly ISteamService _steamService;
+		private readonly IWaxpeerService _waxpeerService;
 
-		public InventoryController(ISteamService steamService)
+		public InventoryController(ISteamService steamService, IWaxpeerService waxpeerService)
 		{
 			_steamService = steamService;
+			_waxpeerService = waxpeerService;
 		}
 
 		[HttpGet("inventory/{steamId}/{steamGame}")]
 		public async Task<IActionResult> Get([Required] long steamId, [Required] SteamGame steamGame, CancellationToken cancellationToken)
 		{
 			var inventory = await _steamService.GetInventoryAsync(steamId, steamGame, cancellationToken);
-			return Ok(inventory);
+			var response = new ApiResponse<List<CounterStrikeAsset>>().Success(inventory);
+			return Ok(response);
 		}
 	}
 }
