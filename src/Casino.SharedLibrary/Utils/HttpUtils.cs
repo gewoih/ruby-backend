ce510694d3a8d373defed73f38671ba2b2af2059
Nikita Ranenko <nranenko@bk.ru>
2023-12-20 21:12:30 +0700

    feat: SharedLibrary added

---
+++ src/Casino.SharedLibrary/Utils/HttpUtils.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace Casino.SharedLibrary.Utils
+{
+    public static class HttpUtils
+    {
+        public const string JsonMediaType = "application/json";
+
+        public static async Task<JObject> Get(IHttpClientFactory httpClientFactory, string requestUrl)
+        {
+            var httpClient = httpClientFactory.CreateClient();
+            var response = await httpClient.GetAsync(requestUrl);
+
+            response.EnsureSuccessStatusCode();
+
+            var stringContent = await response.Content.ReadAsStringAsync();
+            return JObject.Parse(stringContent);
+        }
+
+        public static async Task<JObject> Post(IHttpClientFactory httpClientFactory, string requestUrl, HttpContent content)
+        {
+            var httpClient = httpClientFactory.CreateClient();
+            var response = await httpClient.PostAsync(requestUrl, content);
+
+            response.EnsureSuccessStatusCode();
+
+            var stringContent = await response.Content.ReadAsStringAsync();
+            return JObject.Parse(stringContent);
+        }
+    }
+}
